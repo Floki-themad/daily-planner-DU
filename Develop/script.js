0e3bdd785d97a3020826e5e22c7d30108f4cde1d
Floki <ypr_tylerkeuler@aol.com>
2022-02-15 18:57:46 -0700

    declaring buttons and declaring an object for each task to get stored into based on time

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,48 @@
-function consoleLog(){
-    console.log("it worked")
+// declaring the buttons that i would like to be able to use as well as where to put the current date
+let button = $(".btn");
+let currentDay = $("#currentDay")
 
-}
+// creating an element of time
+let time = moment();
 
-consoleLog
+// declaring an object for each task to get stored into based on the time value
+let schedule = [
+    {
+        taskNumber: 9,
+        taskValue: " ",
+
+    },
+    {
+        taskNumber: 10,
+        taskValue: " ",
+
+    },
+    {
+        taskNumber: 11,
+        taskValue: " ",
+    },
+    {
+        taskNumber: 12,
+        taskValue: " ",
+    },
+    {
+        taskNumber: 1,
+        taskValue: " ",
+    },
+    {
+        taskNumber: 2,
+        taskValue: " ",
+    },
+    {
+        taskNumber: 3,
+        taskValue: " ",
+    },
+    {
+        taskNumber: 4,
+        taskValue: " ",
+    },
+    {
+        taskNumber: 5,
+        taskValue: " ",
+    },
+]
